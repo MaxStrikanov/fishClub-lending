cd691c923b35b46b4d8fc00782ed7280933f44ed
Maksim Strikanov <59795160+MaxStrikanov@users.noreply.github.com>
2023-08-07 01:07:47 +0300

    Update calendatyFish.js

--- js/components/calendatyFish.js
+++ js/components/calendatyFish.js
@@ -6,14 +6,15 @@ export const calendaryFish = async () => {
 	const filterFish =  () => {
 		for(let i = 0; i < percent.length; i++) {
 			let nibble = percentNum[i].textContent.slice(0, -1)
+			let total = 188 - (188 * +nibble) / 100
 		if (nibble > 80) {
-			percent[i].style.cssText = `stroke-dashoffset: calc(250px - (250px * ${nibble}) / 100); stroke: #6BAE37;`	
+			percent[i].style.cssText = `stroke-dashoffset: ${total}; stroke: #6BAE37;`	
 		}
 		else if (65 < nibble < 80) {
-			percent[i].style.cssText = `stroke-dashoffset: calc(250px - (250px * ${nibble}) / 100); stroke: #F19450;`	
+			percent[i].style.cssText = `stroke-dashoffset:${total}; stroke: #F19450;`	
 		}
 		if ( nibble < 65) {
-			percent[i].style.cssText = `stroke-dashoffset: calc(250px - (250px * ${nibble}) / 100); stroke: #F3CD44;`	
+			percent[i].style.cssText = `stroke-dashoffset: ${total}; stroke: #F3CD44;`	
 		}
 	}}
 	filterFish()
